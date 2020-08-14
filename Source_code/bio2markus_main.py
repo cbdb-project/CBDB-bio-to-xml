326bff79be45d5b4011f6f4d333363c617b65621
sudoghut <sudospace@gmail.com>
2020-08-14 11:23:51 +0800

    restructure and add some annos

--- Source_code/bio2markus_main.py
+++ Source_code/bio2markus_main.py
@@ -14,6 +14,9 @@ import bio2markus
 
 #a sample of main() function
 if __name__=='__main__':
+    # The first parameter: 50 means that every 50 files from bio_input will be merged as one single file in markus_output.
+    # The second parameter: 1 means that we will use your file names as markus paragraph ids. 
+    #                       0 means that we will assign markus paragraph ids for you by natural numbers automatically.
     bio_num,serial=50,1
     
     #self-checking before 
